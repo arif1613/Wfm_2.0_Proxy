4674319263251b7780a59f492543354afb651532
arif1613 <arif1613@yahoo.com>
2015-02-25 10:29:25 +0100

    no message

---
+++ Edit/ConcurrencyException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Edit
+{
+    public class ConcurrencyException : Exception
+    {
+        public string StreamName { get; private set; }
+        public IStoredDataVersion ExpectedVersion { get; private set; }
+
+        public ConcurrencyException(string streamName, IStoredDataVersion expectedVersion)
+            : base(string.Format("Expected version {0} in stream '{1}' but it has been changed since", expectedVersion, streamName))
+        {
+            StreamName = streamName;
+            ExpectedVersion = expectedVersion;
+        }
+    }
+}
---
+++ Edit/IStreamStore.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Edit
+{
+    public interface IStreamStore
+    {
+        Task WriteAsync(string streamName, IEnumerable<Chunk> chunks, IStoredDataVersion expectedVersion);
+
+        Task WriteAsync(string streamName, IEnumerable<Chunk> chunks, TimeSpan timeout,
+            IStoredDataVersion expectedVersion);
+
+        Task WriteAsync(string streamName, IEnumerable<Chunk> chunks, CancellationToken token,
+            IStoredDataVersion expectedVersion);
+
+        Task WriteAsync(string streamName, IEnumerable<Chunk> chunks, TimeSpan timeout, CancellationToken token,
+            IStoredDataVersion expectedVersion);
+
+        Task<ChunkSet> ReadAsync(string streamName);
+        Task<ChunkSet> ReadAsync(string streamName, TimeSpan timeout);
+        Task<ChunkSet> ReadAsync(string streamName, CancellationToken token);
+        Task<ChunkSet> ReadAsync(string streamName, TimeSpan timeout, CancellationToken token);
+    }
+
+}
+
